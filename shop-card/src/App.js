692e03307e7506e8b7dcb4d5fe6ebaf3191722da
Samuel Kupoe <62429600+iamkupoe@users.noreply.github.com>
2020-06-28 21:15:41 +0800

    added a prompt alert to the page

--- shop-card/src/App.js
+++ shop-card/src/App.js
@@ -6,7 +6,7 @@ import Sidebar from './Components/sidebar'
 import Body from './Components/Body'
 
 
-
+let test = prompt("Hello, welcome to our page. Enter your name:", " ")
 
 const carts = () => {
   alert('Welcome to our shop')
@@ -15,6 +15,7 @@ const carts = () => {
 function App() {
   return (
     <>
+    
     <button onClick={carts} style={{textAlign:'center', marginLeft:'550px', marginTop:'50px', background:'hotpink', borderRadius:'50px', fontSize:'2em', boxShadow:'5px 10px', borderColor:'none'}}>Kasenagirl Online</button>
     <div className="container" style={{fontFamily:'Inconsolata'}}>
       <div className="row">
